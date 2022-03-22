7be61ff12600cab7a81ba43561a93d3faa19627d
Eric Spencer <spencer22eric@gmail.com>
2022-03-21 22:24:22 -0400

    updated API/added date function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 
 function tempConversion(temp) {
-    return (temp - 273.15) * 9/5 + 32
+    return Math.floor((temp - 273.15) * 9 / 5 + 32)
+}
+
+function populateForecast (response) {
+
+    let temp = tempConversion(response.current.temp);
+    for (let i = 0; i < 5; i++) {
+        let day = response.daily[i]
+
+        let date = new Date(day.dt).toDateString();
+
+        console.log(date);
+    }
 }
 
 function getWeather() {
@@ -10,20 +22,39 @@ function getWeather() {
 
     let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=6479f2af5b538ffcb85e2f9c83f465e0"
 
-    fetch(apiUrl)
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchText + "&limit=2&appid=6479f2af5b538ffcb85e2f9c83f465e0")
         .then(response => response.json())
         .then(function (response) {
 
-            let temp = tempConversion(response.main.temp);
+            let lon = response[0].lon
+            let lat = response[0].lat
 
-            console.log("succeeded");
-            console.log(temp);
+            console.log(lon, lat);
 
+            fetch("http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=6479f2af5b538ffcb85e2f9c83f465e0")
+                .then(response => response.json())
+                .then(function (response) {                    
+
+                    populateForecast(response);
+
+                })
         })
+
+
 };
+
 
 $(document).ready(function () {
 
     $("#search-btn").on("click", getWeather);
+    $("#search").keypress(function (event) {
+
+        let keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode === 13) {
+            getWeather();
+            $("#search").val("")
+        }
+
+    })
 
 });
